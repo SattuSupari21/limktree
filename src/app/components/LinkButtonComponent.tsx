4755d33f53f4fdd927e5ce81aefb1acfdf150959
samtu <41340170+SattuSupari21@users.noreply.github.com>
2024-02-09 21:25:32 +0530

    add feature link reorder
    user can now change order of links by dragging them

--- src/app/components/LinkButtonComponent.tsx
+++ src/app/components/LinkButtonComponent.tsx
@@ -1,6 +1,8 @@
+"use client"
+
 import {Button, CircularProgress} from "@nextui-org/react";
 import React, {useEffect, useMemo, useRef, useState} from "react";
-import {CreateNewLink, DeleteLink, GetUserLinks} from "@/app/actions";
+import {CreateNewLink, DeleteLink, GetUserLinks, UpdateLinksPosition} from "@/app/actions";
 import {FaPlus} from "react-icons/fa6";
 import {Input} from "@nextui-org/input";
 import {buttons} from "@/app/constants";
@@ -10,6 +12,7 @@ import {AiOutlineDelete} from "react-icons/ai";
 import {useRecoilState} from "recoil";
 import {linkState} from "@/store/atoms/links";
 import toast, {Toaster} from "react-hot-toast";
+import {MdDragIndicator} from "react-icons/md";
 
 type LinkButton = {
     key: string,
@@ -25,10 +28,17 @@ type Link = {
     position: number
 }
 
+interface LinkUpdate {
+    id: number,
+    position: number
+}
+
 export default function LinkButtonComponent() {
     const [linksLoading, setLinksLoading] = useRecoilState(linkState);
-    const [links, setLinks] = useState([]);
+    const [links, setLinks] = useState<Link[]>([]);
     const inputRef = useRef(null);
+    const dragLink = useRef<number>(0);
+    const dragOverLink = useRef<number>(0);
     const [activeButtons, setActiveButtons] = useState([]);
     const [availableButtons, setAvailableButtons] = useState(buttons.filter(b => !activeButtons.find((btn: LinkButton) => btn.key === b.key)));
 
@@ -109,6 +119,7 @@ export default function LinkButtonComponent() {
         return (
             <div className="grid grid-cols-4 place-items-center gap-6 mb-2" key={button.key}>
                 <div className="flex gap-2 justify-center items-center mr-auto">
+                    <MdDragIndicator className="cursor-grab"/>
                     <Icon/>
                     <p>{button.label}</p>
                 </div>
@@ -137,16 +148,41 @@ export default function LinkButtonComponent() {
         return <div className="grid place-items-center"><CircularProgress size="lg" aria-label="Loading..."/></div>
     }
 
+
+    function handleSort() {
+        const linksClone: Link[] = [...links]
+        const temp = linksClone[dragLink.current]
+        linksClone[dragLink.current] = linksClone[dragOverLink.current]
+        linksClone[dragOverLink.current] = temp;
+
+        const firstId = linksClone[dragOverLink.current].id;
+        const firstPosition = linksClone[dragOverLink.current].position;
+        const secondId = linksClone[dragLink.current].id;
+        const secondPosition = linksClone[dragLink.current].position;
+
+        setLinks(linksClone)
+
+        UpdateLinksPosition({firstId, firstPosition, secondId, secondPosition}).then(r => r.status);
+    }
+
     return (
         <div className="w-full h-full flex flex-col">
             <div>
-                {links.map((link: Link) => {
+                {links.map((link: Link, index) => {
                     const button: LinkButton = {
                         key: link.id.toString(),
                         label: link.title,
                         icon: buttons.find((b: LinkButton) => b.label === link.title)?.icon
                     }
-                    return <UserLinksComponent button={button} value={link.url}/>
+                    return <div
+                        draggable={true}
+                        onDragStart={() => dragLink.current = index}
+                        onDragEnter={() => dragOverLink.current = index}
+                        onDragEnd={handleSort}
+                        onDragOver={(e) => e.preventDefault()}
+                    ><UserLinksComponent key={button.key} button={button}
+                                         value={link.url}/>
+                    </div>
                 })}
             </div>
             <div>
